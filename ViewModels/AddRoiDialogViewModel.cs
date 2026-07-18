24b8f0c7e2263b9da723f2573d93cae4ef50a714
pathetic-digifan-kr <yangss1993@gmail.com>
2026-07-18 21:13:41 +0900

    refactor:ROI추가 대화창 MvvmToolkit 쓰도록 수정

--- ViewModels/AddRoiDialogViewModel.cs
+++ ViewModels/AddRoiDialogViewModel.cs
@@ -1,66 +1,32 @@
 using System;
-using System.ComponentModel;
-using System.Runtime.CompilerServices;
-using System.Windows.Input;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 
 namespace PdkOcrClient.ViewModels
 {
-    public class AddRoiDialogViewModel : INotifyPropertyChanged
+    public partial class AddRoiDialogViewModel : ObservableObject
     {
         private readonly Action<string?>? _closeAction;
+
+        [ObservableProperty]
         private string? _roiName;
-
-        public event PropertyChangedEventHandler? PropertyChanged;
-
-        public string? RoiName
-        {
-            get => _roiName;
-            set
-            {
-                if (_roiName != value)
-                {
-                    _roiName = value;
-                    OnPropertyChanged();
-                }
-            }
-        }
-
-        public ICommand ConfirmCommand { get; }
-        public ICommand CancelCommand { get; }
 
         public AddRoiDialogViewModel(Action<string?>? closeAction = null)
         {
             _closeAction = closeAction;
-
-            ConfirmCommand = new RelayCommand(() =>
-            {
-                var roiName = string.IsNullOrWhiteSpace(RoiName) ? null : RoiName;
-                _closeAction?.Invoke(roiName);
-            });
-
-            CancelCommand = new RelayCommand(() =>
-            {
-                _closeAction?.Invoke(null);
-            });
         }
 
-        private void OnPropertyChanged([CallerMemberName] string? propertyName = null)
-            => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-
-        private sealed class RelayCommand : ICommand
+        [RelayCommand]
+        private void Confirm()
         {
-            private readonly Action _execute;
+            var roiName = string.IsNullOrWhiteSpace(RoiName) ? null : RoiName;
+            _closeAction?.Invoke(roiName);
+        }
 
-            public RelayCommand(Action execute)
-            {
-                _execute = execute ?? throw new ArgumentNullException(nameof(execute));
-            }
-
-            public event EventHandler? CanExecuteChanged;
-
-            public bool CanExecute(object? parameter) => true;
-
-            public void Execute(object? parameter) => _execute();
+        [RelayCommand]
+        private void Cancel()
+        {
+            _closeAction?.Invoke(null);
         }
     }
 }
