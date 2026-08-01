1308e5ae700e78cfd886c768f52964c6365e2a64
pathetic-digifan-kr <yangss1993@gmail.com>
2026-08-01 20:45:39 +0900

    feat:SelectableRectangle에 선택 기능 추가

--- Views/SelectableRectangle.axaml.cs
+++ Views/SelectableRectangle.axaml.cs
@@ -1,13 +1,139 @@
+using System;
+using System.Collections.Generic;
 using Avalonia;
 using Avalonia.Controls;
-using Avalonia.Markup.Xaml;
+using Avalonia.Controls.Shapes;
+using Avalonia.Input;
+using Avalonia.Media;
+using Avalonia.VisualTree;
 
 namespace PdkOcrClient;
 
 public partial class SelectableRectangle : UserControl
 {
+    private InspectionRegion? _boundRegion;
+
+    public static readonly StyledProperty<bool> IsSelectedProperty =
+        AvaloniaProperty.Register<SelectableRectangle, bool>(nameof(IsSelected));
+
+    public bool IsSelected
+    {
+        get => GetValue(IsSelectedProperty);
+        set => SetValue(IsSelectedProperty, value);
+    }
+
     public SelectableRectangle()
     {
         InitializeComponent();
+        PointerPressed += OnPointerPressed;
+        PropertyChanged += OnPropertyChanged;
+        DataContextChanged += OnDataContextChanged;
+        UpdateSelectionVisualState();
+    }
+
+    private void OnDataContextChanged(object? sender, EventArgs e)
+    {
+        if (_boundRegion is not null)
+        {
+            _boundRegion.PropertyChanged -= OnBoundRegionPropertyChanged;
+        }
+
+        _boundRegion = DataContext as InspectionRegion;
+
+        if (_boundRegion is not null)
+        {
+            SetCurrentValue(IsSelectedProperty, _boundRegion.IsSelected);
+            _boundRegion.PropertyChanged += OnBoundRegionPropertyChanged;
+        }
+        else
+        {
+            SetCurrentValue(IsSelectedProperty, false);
+        }
+
+        UpdateSelectionVisualState();
+    }
+
+    private void OnBoundRegionPropertyChanged(object? sender, System.ComponentModel.PropertyChangedEventArgs e)
+    {
+        if (e.PropertyName == nameof(InspectionRegion.IsSelected))
+        {
+            UpdateSelectionVisualState();
+            SetCurrentValue(IsSelectedProperty, _boundRegion?.IsSelected ?? false);
+        }
+    }
+
+    private void OnPropertyChanged(object? sender, AvaloniaPropertyChangedEventArgs e)
+    {
+        if (e.Property == IsSelectedProperty)
+        {
+            UpdateSelectionVisualState();
+
+            if (DataContext is InspectionRegion region)
+            {
+                region.IsSelected = (bool)e.NewValue!;
+            }
+        }
+    }
+
+    private void OnPointerPressed(object? sender, PointerPressedEventArgs e)
+    {
+        if (DataContext is not InspectionRegion currentRegion)
+        {
+            return;
+        }
+
+        var parentItemsControl = this.GetVisualParent<ItemsControl>();
+        if (parentItemsControl?.ItemsSource is IEnumerable<InspectionRegion> regions)
+        {
+            foreach (var region in regions)
+            {
+                region.IsSelected = ReferenceEquals(region, currentRegion);
+            }
+        }
+        else
+        {
+            currentRegion.IsSelected = true;
+        }
+
+        SetCurrentValue(IsSelectedProperty, true);
+        e.Handled = true;
+    }
+
+    private void UpdateSelectionVisualState()
+    {
+        var border = this.FindControl<Border>("SelectionBorder");
+        var fill = this.FindControl<Rectangle>("RegionFill");
+        var highlight = this.FindControl<Rectangle>("SelectionHighlight");
+
+        var isSelected = IsSelected;
+        if (DataContext is InspectionRegion region)
+        {
+            isSelected = region.IsSelected;
+        }
+
+        var selectedBorderBrush = new SolidColorBrush(Color.Parse("#38BDF8"));
+        var defaultBorderBrush = new SolidColorBrush(Color.Parse("#2563EB"));
+        var selectedFillBrush = new SolidColorBrush(Color.Parse("#38BDF8"));
+        var defaultFillBrush = new SolidColorBrush(Color.Parse("#2563EB"));
+
+        if (border is not null)
+        {
+            border.BorderBrush = isSelected ? selectedBorderBrush : defaultBorderBrush;
+            border.BorderThickness = isSelected ? new Thickness(2) : new Thickness(1);
+            border.Padding = isSelected ? new Thickness(3) : new Thickness(2);
+        }
+
+        if (fill is not null)
+        {
+            fill.Stroke = isSelected ? selectedFillBrush : defaultFillBrush;
+            fill.Fill = isSelected ? selectedFillBrush : defaultFillBrush;
+            fill.Opacity = isSelected ? 0.24 : 0.18;
+            fill.StrokeThickness = isSelected ? 3 : 2;
+        }
+
+        if (highlight is not null)
+        {
+            highlight.IsVisible = isSelected;
+        }
     }
 }
