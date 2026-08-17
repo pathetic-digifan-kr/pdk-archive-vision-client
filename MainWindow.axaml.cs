652a0dcac42c96187cc3d7a41c1ce53866fe1b5e
pathetic-digifan-kr <yangss1993@gmail.com>
2026-08-17 10:16:44 +0900

    fix:드래그를 해서 사각형을 그리는 기능이 사라진 문제 수정

--- MainWindow.axaml.cs
+++ MainWindow.axaml.cs
@@ -13,6 +13,11 @@ public partial class MainWindow : Window
         MovingRegion
     }
 
+    private RoiInteractionState _state = RoiInteractionState.None;
+
+    private Point _roiStartPoint;
+    private Point _roiCurrentPoint;
+
     public static readonly StyledProperty<object?> SelectedRectangleProperty =
         AvaloniaProperty.Register<MainWindow, object?>(nameof(SelectedRectangle));
 
@@ -38,4 +43,39 @@ public partial class MainWindow : Window
         SetCurrentValue(SelectedRectangleProperty, selectedRectangle.DataContext);
     }
 
+    private void RoiCanvas_PointerReleased(object? sender, Avalonia.Input.PointerReleasedEventArgs e)
+    {
+        _state = RoiInteractionState.None;
+    }
+
+    private void RoiCanvas_PointerMoved(object? sender, Avalonia.Input.PointerEventArgs e)
+    {
+        if (_state != RoiInteractionState.DrawingNewRegion) return;
+
+        _roiCurrentPoint = e.GetPosition(RoiCanvas);
+        UpdateRoiRectangleVisual();
+    }
+
+    private void RoiCanvas_PointerPressed(object? sender, Avalonia.Input.PointerPressedEventArgs e)
+    {
+        _state = RoiInteractionState.DrawingNewRegion;
+        _roiStartPoint = e.GetPosition(RoiCanvas);
+        _roiCurrentPoint = _roiStartPoint;
+        UpdateRoiRectangleVisual();
+    }
+
+
+    private void UpdateRoiRectangleVisual()
+    {
+        var x = Math.Min(_roiStartPoint.X, _roiCurrentPoint.X);
+        var y = Math.Min(_roiStartPoint.Y, _roiCurrentPoint.Y);
+        var width = Math.Abs(_roiCurrentPoint.X - _roiStartPoint.X);
+        var height = Math.Abs(_roiCurrentPoint.Y - _roiStartPoint.Y);
+
+        Canvas.SetLeft(RoiRectangle, x);
+        Canvas.SetTop(RoiRectangle, y);
+        RoiRectangle.Width = width;
+        RoiRectangle.Height = height;
+        RoiRectangle.IsVisible = width > 0 && height > 0;
+    }
 }
