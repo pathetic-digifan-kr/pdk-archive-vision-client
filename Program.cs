0bb0534a43b0f54e9ec72216519f75d35c2fa66b
pathetic-digifan-kr <yangss1993@gmail.com>
2026-09-04 19:11:38 +0900

    fix:ubuntu wayland에서 한글 입력이 안되는 문제 수정 및 한글 폰트 추가

--- Program.cs
+++ Program.cs
@@ -1,5 +1,7 @@
 ﻿using Avalonia;
+using Avalonia.Media;
 using System;
+using System.Collections.Generic;
 
 namespace PdkOcrClient;
 
@@ -14,8 +16,45 @@ class Program
 
     // Avalonia configuration, don't remove; also used by visual designer.
     public static AppBuilder BuildAvaloniaApp()
-        => AppBuilder.Configure<App>()
+    {
+        var appBuilder = AppBuilder.Configure<App>()
             .UsePlatformDetect()
             .WithInterFont()
-            .LogToTrace();
+            .LogToTrace()
+            .With(new FontManagerOptions
+            {
+                FontFallbacks =
+                [
+                    new FontFallback { FontFamily = new FontFamily("avares://PdkOcrClient/Assets/Fonts#NanumGothic") }
+                ]
+            });
+            
+        if(IsUbuntuWayland())
+        {
+            appBuilder = appBuilder.UseWayland();
+        }
+        else
+        {
+            appBuilder = appBuilder.UsePlatformDetect();
+        }
+                    
+        return appBuilder;
+    }
+        
+          
+
+    public static bool IsUbuntuWayland()
+    {
+        if (!OperatingSystem.IsLinux()) 
+            return false;
+
+        // 1. 세션 타입 체크 (wayland)
+        var sessionType = Environment.GetEnvironmentVariable("XDG_SESSION_TYPE");
+        
+        // 2. Wayland 디스플레이 소켓 존재 여부 체크 (e.g., wayland-0)
+        var waylandDisplay = Environment.GetEnvironmentVariable("WAYLAND_DISPLAY");
+
+        return string.Equals(sessionType, "wayland", StringComparison.OrdinalIgnoreCase) 
+            || !string.IsNullOrEmpty(waylandDisplay);
+    }
 }
